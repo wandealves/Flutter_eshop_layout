4f9533a1ceac39d94ab2bb648716c61546e5d838
Wanderson <wanderson.alves.rodrigues@gmail.com>
2019-12-28 11:27:02 -0300

    Criação do product

--- eshop/lib/pages/home.page.dart
+++ eshop/lib/pages/home.page.dart
@@ -1,3 +1,4 @@
+import 'package:eshop/pages/product.page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -55,7 +56,7 @@ class HomePage extends StatelessWidget {
                   ),
                   Container(
                     height: 350,
-                    child: productList(),
+                    child: productList(context),
                   ),
                 ],
               ),
@@ -154,24 +155,24 @@ Widget categoryItem() {
   );
 }
 
-Widget productList() {
+Widget productList(BuildContext context) {
   return Container(
     height: double.infinity,
     child: ListView(
       scrollDirection: Axis.horizontal,
       children: <Widget>[
-        productItem(),
-        productItem(),
-        productItem(),
-        productItem(),
-        productItem(),
-        productItem(),
+        productItem(context),
+        productItem(context),
+        productItem(context),
+        productItem(context),
+        productItem(context),
+        productItem(context),
       ],
     ),
   );
 }
 
-Widget productItem() {
+Widget productItem(BuildContext context) {
   return Container(
     padding: EdgeInsets.all(10),
     margin: EdgeInsets.all(5),
@@ -180,11 +181,21 @@ Widget productItem() {
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Image.asset(
-          "assets/product-1.png",
-          width: 170,
-          height: 170,
-          fit: BoxFit.cover,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ProductPage(),
+              ),
+            );
+          },
+          child: Image.asset(
+            "assets/product-1.png",
+            width: 170,
+            height: 170,
+            fit: BoxFit.cover,
+          ),
         ),
         SizedBox(
           height: 10,
